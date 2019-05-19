6140674f65b224eaf336bcecf6879300a20b835b
me_home <dmitrijs.kass@gmail.com>
2019-05-19 19:44:44 +0300

    Source all code from R script in 1 line.

--- content/post/2019-05-08-swdchallenge-artisanal-data.R
+++ content/post/2019-05-08-swdchallenge-artisanal-data.R
@@ -1,7 +1,30 @@
 
+# Packages ----
+
 library(tidyverse)
 library(ggrepel)
 library(scales)
+
+
+
+
+# Plot the relationship between a number of documents with term t in it and the term’s idf.
+
+p_idf <- tibble(n_docs_with_term = 1:12,
+                idf = log(12 / n_docs_with_term)) %>% 
+  ggplot(aes(x = n_docs_with_term, y = idf)) +
+  geom_point(size = 2) +
+  geom_line() +
+  labs(x = "Number of documents with term t in it",
+       y = "idf(t)") +
+  scale_y_continuous(breaks = seq(0, 3, by = 0.5), minor_breaks = NULL, expand = expand_scale(add = 0.05)) +
+  scale_x_continuous(breaks = 1:12) +
+  theme_classic() +
+  theme(panel.grid = element_blank(), 
+        axis.text.x = element_text(size = 10, colour = "grey30"),
+        axis.text.y = element_text(size = 10, colour = "grey30"), 
+        axis.title = element_text(size = 10, colour = "grey30"), 
+        plot.title = element_text(size = 11, colour = "grey30", hjust = 0))
 
 
 
@@ -45,11 +68,10 @@ content_df <- tibble(paragraph_id = 1:length(content),
 content_df$paragraph <- str_replace_all(string = content_df$paragraph,
                                         pattern = gtools::chr(146), # https://en.wikipedia.org/wiki/Windows-1252
                                         replacement = gtools::chr(39))
+
 content_df$paragraph <- qdap::replace_contraction(content_df$paragraph)
 
-# Steps 3., 4., 5. Tokenize, lemmatize, mark stop words.
-
-# Define stop words.
+# Steps 3. Tokenize, Step 4. Lemmatize, Step 5. Mark stop words.
 
 # Know your stop words. For example, the SMART set (as documented in Appendix 11
 # of http://jmlr.csail.mit.edu/papers/volume5/lewis04a/) has all single letters
@@ -75,7 +97,7 @@ tokens <- tokens %>%
 
 
 
-# Term frequency (tf) ----
+# Tf ----
 
 # Calculate word frequency.
 tokens_freq <- tokens %>% 
@@ -101,16 +123,44 @@ tokens_freq <- tokens_freq %>%
 tokens_tfidf <- tokens_freq %>% 
   tidytext::bind_tf_idf(word_lemma, interview, n)
 
-# View words with zero tf-idf.
-tokens_tfidf %>% 
+# Extract words with zero tf-idf.
+zero_tfidf <- tokens_tfidf %>% 
   filter(tf_idf == 0) %>% 
   pull(word_lemma) %>% 
-  unique()
+  unique() %>% 
+  paste(sep = ",")
+
+# How many documents contain each word.
+terms_in_documents <- tokens_tfidf %>% 
+  group_by(word_lemma) %>% 
+  summarise(documents = n_distinct(interview))
+
+# Data frame for viz.
+slopes_tf_tfidf <- tokens_tfidf %>% 
+  filter(interview == "Andy Cotgreave") %>% 
+  add_count(name = "total_words", wt = n) %>% 
+  # arrange(desc(word_lemma)) %>% 
+  mutate(word_tf_rank = row_number(-tf),
+         word_tfidf_rank = row_number(-tf_idf)) %>%
+  filter(word_tf_rank %in% 1:6 | word_tfidf_rank %in% 1:6) %>% 
+  ungroup()
+
+# Summary table with most important words by tf and tf-idf.
+t_summary <- slopes_tf_tfidf %>% 
+  left_join(terms_in_documents, by = "word_lemma") %>% 
+  select(word_lemma, freq = n, total_words, tf, documents, idf, tf_idf, tf_rank = word_tf_rank, tfidf_rank = word_tfidf_rank) %>% 
+  arrange(desc(freq), desc(tfidf_rank))
 
 
 
 
 # Visualize the effect of idf on tf ----
+
+# Transformations for the plot.
+slopes_tf_tfidf_gathered <- slopes_tf_tfidf %>% 
+  select(interview, word_lemma, word_tf_rank, word_tfidf_rank) %>% 
+  mutate(rank_inc = word_tf_rank > word_tfidf_rank) %>% 
+  gather(key, value, word_tf_rank, word_tfidf_rank)
 
 # Colours.
 swd_light_blue <- "#95b3d7"
@@ -125,66 +175,52 @@ reverselog_trans <- function(base = exp(1)) {
             domain = c(1e-100, Inf))
 }
 
-# Data frame for viz.
-slopes_tf_tfidf <- tokens_tfidf %>% 
-  filter(interview == "Andy Cotgreave") %>% 
-  mutate(word_tf_rank = row_number(-tf),
-         word_tfidf_rank = row_number(-tf_idf)) %>%
-  filter(word_tf_rank %in% 1:6 | word_tfidf_rank %in% 1:6) %>% 
-  ungroup() %>% 
-  select(interview, word_lemma, word_tf_rank, word_tfidf_rank) %>% 
-  mutate(rank_inc = word_tf_rank > word_tfidf_rank) %>% 
-  gather(key, value, word_tf_rank, word_tfidf_rank)
-
 # Plot titles.
-gg_title <- 'tf-idf: the effect of idf on tf.'
-gg_subtitle <- paste(str_wrap(
-  "As an example, these are some of words used by Andy Cotgreave in the 'learning dataviz' episode of #SWDpodast. Although words such as 'data' and 'learn' are the 1st and the 5th most frequent, they are not important when compared to other 11 interviews on the same podcast episode. This is because all 12 interviews are about learning data visualization.", width = 77),
-  "",
-  str_wrap("Notice how idf diminishes the weights of words that appear in many interviews (e.g. 'data', 'learn') and promotes other words (e.g. 'copy', 'fun') as more relevant for a particular interview.", width = 77),
-  "",
-  str_wrap("A word 'copy' is important in the context of recommending Austin Kleon's book Steal Like an Artist. A word 'fun' is important in recommending the listeners to 'get out of your comfort zone and have fun'. 'Have fun. My gosh, you are allowed to have fun!' invites Andy.", width = 77),
-  sep = "\n")
-
+gg_title <- 'tf-idf: the effect of idf on tf'
+gg_subtitle <- "Comparison of two measures of word importance: tf (term frequency) and tf-idf (term frequency-inverse document frequency). Ranks are positions relative to other words. Highest rank means highest importance. Lowest rank is 271."
 gg_caption <- "Source: transcript of #SWD podcast episode 'learning dataviz'.\nIllustration by @DmitrijsKass in R with ggplot2."
 
 # Viz.
-set.seed(3) # for `geom_text_repel`.
-slopes_tf_tfidf %>% 
+set.seed(1)
+p_slopes <- slopes_tf_tfidf_gathered %>% 
   ggplot(aes(x = key, y = value, group = word_lemma, label = word_lemma)) +
   geom_point(colour = swd_light_blue, size = 2) +
-  geom_point(data = filter(slopes_tf_tfidf, rank_inc == TRUE), colour = swd_dark_blue, size = 2) +
+  geom_point(data = filter(slopes_tf_tfidf_gathered, rank_inc == TRUE), colour = swd_dark_blue, size = 2) +
   geom_line(colour = swd_light_blue) +
-  geom_line(data = filter(slopes_tf_tfidf, rank_inc == TRUE), colour = swd_dark_blue, size = 1.05) +
-  geom_text_repel(data = filter(slopes_tf_tfidf, key == "word_tf_rank"), 
+  geom_line(data = filter(slopes_tf_tfidf_gathered, rank_inc == TRUE), colour = swd_dark_blue, size = 1.05) +
+  geom_text_repel(data = filter(slopes_tf_tfidf_gathered, key == "word_tf_rank"), 
                   mapping = aes(color = rank_inc),
                   nudge_x = -0.02, 
                   hjust = "outward",
+                  segment.colour = "grey20",
                   show.legend = FALSE) +
-  geom_text_repel(data = filter(slopes_tf_tfidf, key != "word_tf_rank"), 
+  geom_text_repel(data = filter(slopes_tf_tfidf_gathered, key != "word_tf_rank"), 
                   mapping = aes(color = rank_inc),
                   nudge_x = +0.02, 
                   hjust = "outward",
+                  segment.colour = "grey20",
                   show.legend = FALSE) +
   scale_color_manual(values = c("TRUE" = swd_dark_blue, "FALSE" = swd_light_blue)) +
   scale_y_continuous(trans = reverselog_trans(10), 
-                     sec.axis = dup_axis(), 
+                     sec.axis = dup_axis(name = str_wrap("rank of words by tf-idf", width = 8)), 
                      breaks = c(1:3, 5, 10, 50, 100, 300), 
                      minor_breaks = NULL) +
-  scale_x_discrete(labels = c("rank of words\nby tf\n", "rank of words\nby tf-idf\n"), 
+  scale_x_discrete(labels = NULL, 
                    position = "top", 
-                   expand = expand_scale(mult = 0.2, add = 0)) +
+                   expand = expand_scale(mult = 0.25, add = 0)) +
   labs(x = NULL,
-       y = NULL,
+       y = str_wrap("rank of words by tf", width = 8),
        title = gg_title,
-       subtitle = gg_subtitle,
+       subtitle = str_wrap(gg_subtitle, width = 77),
        caption = gg_caption) +
   theme_minimal() +
   theme(panel.grid = element_blank(), 
+        axis.title.y.left = element_text(angle = 0, colour = "grey30", vjust = 0.97),
+        axis.title.y.right = element_text(angle = 0, colour = "grey30", vjust = 0.97),
         axis.text.x = element_text(size = 12, colour = "grey30"),
         axis.text.y = element_text(size = 10, colour = "grey30"), 
-        plot.title = element_text(size = 15, colour = "grey30", hjust = 0.5),
-        plot.subtitle = element_text(size = 9.5, colour = "grey40", margin = margin(b = 15, t = 15)),
+        plot.title = element_text(size = 15, colour = "grey30", hjust = 0.5, margin = margin(b = 0)),
+        plot.subtitle = element_text(size = 12, colour = "grey50", hjust = 0.5, margin = margin(t = 10, b = 15)),
         plot.caption = element_text(size = 8, colour = "grey40", hjust = 0.5, margin = margin(t = 15)))
 
-ggsave(filename = "tfidf-the-effect-of-idf-on-tf.png", width = 5.2, height = 7.8, units = "in")
+ggsave(plot = p_slopes, filename = "tfidf-the-effect-of-idf-on-tf.png", width = 6, height = 7.8, units = "in")

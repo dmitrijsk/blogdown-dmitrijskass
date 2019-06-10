618a0497d22910142ed3649cc5133dda6ed2001f
me_home <dmitrijs.kass@gmail.com>
2019-06-11 00:45:47 +0300

    final plot after submission. code NOT polished.

--- static/post/2019-06-10-branding-your-graph-with-ggplot2/2019-06-10-branding-your-graph-with-ggplot2.R
+++ static/post/2019-06-10-branding-your-graph-with-ggplot2/2019-06-10-branding-your-graph-with-ggplot2.R
@@ -1,14 +1,27 @@
 library(tidyverse)
 library(lubridate)
 library(scales)
+library(grid)
+library(gridExtra)
+
+# IKEA brand guideline.
+# https://issuu.com/jemjemjemjem/docs/ikea
+
+
+
+# Data import ----
 
 # Import given data.
+# Source: http://www.storytellingwithdata.com/blog/2019/6/03/swdchallenge-rebrand-it
 df_orig <- readxl::read_xlsx("SWDchallenge_June19.xlsx", range = "B3:Z7")
 
 # Import a file comments.
-points_comments <- read_delim("2019-06-10-poinits-and-comments.csv", delim = ";")
+points_comments <- read_delim("SWDchallenge_June19_comments.csv", delim = ";")
 
-# Tidy and combine tables.
+
+
+# Tidy and combine tables ----
+
 df <- as_tibble(t(df_orig)) %>% 
   select(is_actual = V1, year = V2, month = V3, sales = V4) %>% 
   fill(is_actual, year, .direction = "down") %>% 
@@ -27,14 +40,15 @@ df <- as_tibble(t(df_orig)) %>%
                                 true = dollar(sales, accuracy = 0.1, suffix = "B"), 
                                 false = NA_character_))
 
-# Brand colours.
+
+
+# Plot ----
+
+# IKEA brand colours.
 cols <- c("blue" = "#006bb5", "yellow" = "#ffe81e")
 
-# Extra fonts: https://stackoverflow.com/questions/34522732/changing-fonts-in-ggplot2
+# Extra fonts.
 extrafont::loadfonts(device="win", quiet = TRUE) # Registers fonts so that they can be used in plots. Must be run once in each R session.
-
-# linetype for actual/forecast - DONE
-# points - DONE
 
 p <- df %>% 
   ggplot(aes(x = date, y = sales, group = 1)) +
@@ -61,7 +75,7 @@ p <- df %>%
   geom_point(data = df %>% filter(date %in% points_comments$date), 
              shape = 21, 
              size = 4, 
-             colour = "white",
+             colour = cols["blue"],
              fill = cols["yellow"]) +
   scale_y_continuous(breaks = NULL, expand = c(0, 0)) +
   scale_x_date(breaks = "1 month", 
@@ -70,9 +84,9 @@ p <- df %>%
                expand = expand_scale(0, add = 15)) +
   labs(x = NULL, 
        y = NULL,
-       title = "Market size over time",
+       title = NULL,
        subtitle = "Sales in billions ($USD), monthly",
-       caption = "Makeover in R by Dmitrijs Kass @dmitrijsk for #SWDChallenge") +
+       caption = "Makeover in R by Dmitrijs Kass @dmitrijskass for #SWDChallenge. Disclaimer: This is fake data for an illustrative branding exercise.") +
   theme_classic() +
   theme(axis.text.x.bottom = element_text(hjust = 0),
         axis.line.y = element_blank(),
@@ -80,31 +94,33 @@ p <- df %>%
         plot.title = element_text(family = "Open Sans", face = "bold"),
         plot.subtitle = element_text(family = "Playfair Display", margin = margin(b = 3, unit = "lines")),
         plot.caption = element_text(hjust = 0, size = 7), 
-        plot.margin = margin(0.5, 0.5, 0.5, 1, "lines")) + 
+        plot.margin = margin(0.5, 0.5, 0.5, 1, "lines"), 
+        plot.background = element_rect(fill = "#F6F6F6"),
+        panel.background = element_rect(fill = "#F6F6F6"),
+        panel.border = element_blank()) + 
   coord_cartesian(ylim = c(0, 3), clip = "off") +
-  annotate("point", x = max(df$date), y = 3.7, shape = 15, size = 3) +
-  annotate("point", x = max(df$date) + 15, y = 3.85, shape = 19, size = 3)
+  annotate("point", x = max(df$date), y = 3.93, shape = 15, size = 3, colour = cols["blue"]) +
+  annotate("point", x = max(df$date) + 15, y = 4.08, shape = 19, size = 3, colour = cols["blue"])
 
 p
-ggimage::ggbackground(p, "trees-1915248_1920_greyscale2.jpg")
-ggsave(filename = "ikea_split_labels2.png", height = 10, width = 10 * 1.78, units = "cm", dpi = 600)
+
+g_title <- grobTree(rectGrob(gp = gpar(fill = cols["yellow"], 
+                                       col = "#F6F6F6")),
+                    textGrob(label = "Market size over time", 
+                             x = 0.027, 
+                             hjust = 0,
+                             gp = gpar(col = cols["blue"], 
+                                       cex = 1.2,
+                                       fontfamily = "Open Sans",
+                                       fontface = "bold")))
+
+p_with_title <- grid.arrange(g_title, p, heights = c(1, 13))
+
+ggsave(plot = p_with_title, filename = "swdchallenge-rebrand-it-ikea.png", height = 10, width = 10 * 1.78, units = "cm", dpi = 600)
 
 
 
-
-
-# extrafont::font_import(pattern="PlayfairDisplay-Regular") # this not sure
-
-# windowsFonts(sans="Raleway") # this helped???
-
-loadfonts(device="postscript") # this not necessary
-fonttable() # also helped - see column "FullName"
-extrafont::font_import(pattern="PlayfairDisplay-Bold")
-# helped: https://blog.revolutionanalytics.com/2012/09/how-to-use-your-favorite-fonts-in-r-charts.html
-
-# Get colours from image: https://imagecolorpicker.com/
-
-# Change non-data elements to match SWD's graph.
+# Basic plot chechlist ----
 
 # axis names - DONE
 # x-axis labels - DONE
@@ -112,27 +128,40 @@ extrafont::font_import(pattern="PlayfairDisplay-Bold")
 # title - DONE
 # caption - DONE
 # comments - DONE
+# linetype for actual/forecast - DONE
+# points - DONE
 
-# Branding:
+
+# Questions to myself before branding ----
 
 # What are default fonts in `theme_grey()`?
 # Do I have fonts for IKEA branding?
 # How to change default fonts to IKEA fonts?
 # What are other style changes that are necessary to make the graph look like IKEA's own?
-             
 
 
-# https://guangchuangyu.github.io/2018/04/setting-ggplot2-background-with-ggbackground/
-# ggimage::ggbackground(p3_branded, "background-hardwood-smooth-301717.jpg")
-
-# add logo
-# https://cran.r-project.org/web/packages/magick/vignettes/intro.html
 
 
-# super-categity for x-axis
+# Links used in the process ----
+
+# Fonts.
+# Helped: https://stackoverflow.com/questions/34522732/changing-fonts-in-ggplot2
+# Good blog post: https://blog.revolutionanalytics.com/2012/09/how-to-use-your-favorite-fonts-in-r-charts.html
+# extrafont::fonttable() # Helpful to find the "FullName" of the font.
+
+# Super-categories for x-axis (in the end came up with a simple solution with labels).
 # https://stackoverflow.com/questions/48552671/ggplot2-show-category-and-sub-category-for-x-axis-labels
 # https://stackoverflow.com/questions/44247239/grouping-on-the-x-axis-in-ggplot2
 # https://stackoverflow.com/questions/20571306/multi-row-x-axis-labels-in-ggplot-line-chart
 # https://stackoverflow.com/questions/44616530/axis-labels-on-two-lines-with-nested-x-variables-year-below-months/44616739
 # https://github.com/tidyverse/ggplot2/issues/1966
 
+# Add logo.
+# https://cran.r-project.org/web/packages/magick/vignettes/intro.html
+
+# Get colours from image (web or local).
+# https://imagecolorpicker.com/
+
+# Add image to the background.
+# https://guangchuangyu.github.io/2018/04/setting-ggplot2-background-with-ggbackground/
+# ggimage::ggbackground(p, "filename.png")

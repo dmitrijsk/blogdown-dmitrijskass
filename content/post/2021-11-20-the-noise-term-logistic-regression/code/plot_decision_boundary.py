1658f6280e3cfece8a901e3515ab5eb8cf041519
Dmitrijs Kass <dmitrijs.kass@gmail.com>
2021-11-20 21:45:02 +0100

    Text, code and image.

---
+++ content/post/2021-11-20-the-noise-term-logistic-regression/code/plot_decision_boundary.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
+def plot_decision_boundary(X, y, model):
+    
+    # Generate a grid of points and classify them ----------------------------
+    
+    # Create a grid of points.
+    res = 0.01 # Resolution of the decision boundary.
+    xs1 = np.arange(0, 1 + res, res)
+    xs2 = np.arange(0, 1 + res, res)
+    xs1, xs2 = np.meshgrid(xs1, xs2)
+    X_grid = np.column_stack((xs1.flatten(), xs2.flatten()))
+    # Predict a class for each point of the grid.
+    pred_grid = model.predict(X_grid)
+    
+    # Make a plot ------------------------------------------------------------
+    
+    fig, ax = plt.subplots(figsize = (5, 5), dpi = 300)
+    ax.set(xlim = [0,1], 
+           ylim = [0,1], 
+           xlabel = '$x_1$', 
+           ylabel = '$x_2$', 
+           title = 'Logistic regression decision boundary')
+    # Plot data points.
+    ax.scatter(X[:,0], X[:,1], c = y, cmap='bwr') # Blue-white-red colormap.
+    # Plot class regions.
+    cmap = LinearSegmentedColormap.from_list('cmap', ['skyblue', 'lightsalmon'])
+    ax.imshow(pred_grid.reshape(xs1.shape[0],-1),
+               cmap = cmap,
+               origin = 'lower',
+               extent = [0,1,0,1],
+               aspect = 'auto', alpha = 0.6)    
+    fig.savefig("log-reg-decision-boundary.png")
+    
---
+++ content/post/2021-11-20-the-noise-term-logistic-regression/code/simulate_data_for_logistic_regression.py
@@ -0,0 +1,38 @@
+import numpy as np
+from sklearn.linear_model import LogisticRegression
+from plot_decision_boundary import plot_decision_boundary as plot_db
+
+def logistic(z):
+    return 1 / (1 + np.exp(-z))
+
+# True theta coefficients.
+theta = np.array([4, -2])
+# Number of features.
+p = len(theta)
+
+# Number of training data points.
+n = 200
+# Generate feature values from U[0,1].
+np.random.seed(1)
+X = np.random.rand(n, p)
+
+# Calculate logits.
+z = X @ theta.reshape(-1, 1)
+# Calculate probabilities.
+prob = logistic(z)
+
+# Generate labels by sampling from Bernoulli(prob)
+y = np.random.binomial(1, prob.flatten())
+
+# Check the proportion of classes.
+y_counts = np.unique(y, return_counts = True)
+print(f"Proportions of classes: {y_counts[1] / n}")
+
+# Train a logistic regression model.
+model = LogisticRegression(fit_intercept = False, penalty = "none").fit(X, y)
+
+# Compare the learned parameters to the true ones.
+print(f"Learned theta: {np.round(model.coef_, 2)} (true theta was {theta})")
+
+# Plot decision boundaries.
+plot_db(X, y, model)
